87ea04dbcd811997fa2e6c391cd191b77bcf1059
ArmandoBSilva99 <armandob.silva99@gmail.com>
2022-05-21 19:04:54 +0100

    avanços

--- src/sdstored.c
+++ src/sdstored.c
@@ -30,7 +30,7 @@ ssize_t myreadln(int fildes, void* buf, size_t nbyte){
     return size;
 }
 
-void exec_filtros(char* filtro){
+void exec_commands(char* transformation){
 
     char nop[100];
     sprintf(nop,"%s/nop",filters_folder);
@@ -54,26 +54,126 @@ void exec_filtros(char* filtro){
     char decrypt[100]; 
     sprintf(decrypt,"%s/decrypt",filters_folder);
 
-    if (strcmp(filtro,"nop") == 0)        
+    if (strcmp(transformation,"nop") == 0)        
         execl(nop,"nop", NULL);
     
-    else if (strcmp(filtro,"bcompress") == 0)
+    else if (strcmp(transformation,"bcompress") == 0)
         execl(bcompress,"bcompress", NULL);
     
-    else if (strcmp(filtro,"bdecompress") == 0)
+    else if (strcmp(transformation,"bdecompress") == 0)
         execl(bdecompress,"bdecompress", NULL);
     
-    else if (strcmp(filtro,"gcompress") == 0)
+    else if (strcmp(transformation,"gcompress") == 0)
         execl(gcompress,"gcompress", NULL);
     
-    else if (strcmp(filtro,"gdecompress") == 0)
+    else if (strcmp(transformation,"gdecompress") == 0)
         execl(gdecompress,"gdecompress", NULL);
     
-    else if (strcmp(filtro,"encrypt") == 0)
+    else if (strcmp(transformation,"encrypt") == 0)
         execl(encrypt,"encrypt", NULL);
     
-    else if (strcmp(filtro,"decrypt") == 0)
+    else if (strcmp(transformation,"decrypt") == 0)
         execl(decrypt,"decrypt", NULL);    
+}
+
+void transform(char* input_file, char* output_file, char** transformations){
+
+    int fd_in = open(input_file, O_RDONLY, 0644);
+    int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    
+    int num_transformations;
+    for(num_transformations = 0; transformations[num_transformations] != NULL; num_transformations++);
+
+    int p[num_transformations - 1][2];
+
+    int status[num_transformations];
+
+    if(num_transformations > 1) {
+        for(int i = 0; i < num_transformations; i++){
+
+            //First Transformation
+            if (i == 0){
+                pipe(p[i]);
+                if (fork()){
+                    dup2(fd_in, 0);
+                    close(fd_in);
+
+                    close(p[i][0]);
+                    
+                    dup2(p[i][1], 1);
+                    close(p[i][1]);
+                    
+                    exec_commands(transformations[i]);
+
+                    _exit(0);
+                }
+                else {
+                    close(p[i][1]);                
+                }
+            }
+
+            //Last Transformation
+            else if (i == num_transformations - 1){
+                if (fork()){
+
+                    dup2(fd_out, 1);
+                    close(fd_out);
+
+                    dup2(p[i-1][0],0);
+                    close(p[i-1][0]);
+
+                    exec_commands(transformations[i]);
+                    
+                    _exit(0);
+
+                }
+                else {
+                    close(p[i-1][0]);
+                }
+            }
+
+            //In between Transformations 
+            else {
+                pipe(p[i]);
+                if (fork()){
+                    close(p[i][0]);
+
+                    dup2(p[i][1], 1);
+                    close(p[i][1]);
+
+                    dup2(p[i-1][0], 0);
+                    close(p[i-1][0]);
+
+                    exec_commands(transformations[i]);
+
+                    _exit(0);
+                }
+                else {
+                    close(p[i][1]);
+                    close(p[i-1][0]);
+                }
+            }
+        }
+
+        for(int w = 0; w < num_transformations; w++)
+            wait(&status[w]);
+    }
+    else {
+        if(fork()){
+            dup2(fd_in, 0);
+            dup2(fd_out, 1);
+            close(fd_in);
+            close(fd_out);
+
+            exec_commands(transformations[0]);
+
+            _exit(0);
+        }
+        else {
+            wait(&status[0]);
+        }
+    }        
+
 }
 
 
@@ -100,7 +200,6 @@ void parseTransf(char* filepath){
 
     close(fd);
 }
-
 
 int main(int argc, char* argv[]){
 
@@ -133,7 +232,34 @@ int main(int argc, char* argv[]){
         printf("INSIDE FIFO: %s\n", buffer);
 
         if(strncmp(buffer, "proc-file", 9) == 0){
-            // DO THINGS
+            char* args = buffer + 10; // removes proc-file and space
+
+            /*
+            * PARSING ARGS 
+            */
+            char* token = strtok(args," ");
+            char* input_files = malloc(sizeof(char*));
+            char* output_files = malloc(sizeof(char*));
+            strcpy(input_files, token);
+            token = strtok(NULL, " ");
+            strcpy(output_files, token);
+            printf("INPUT FILE: %s\nOUTPUT FILE: %s\n", input_files, output_files);
+            token = strtok(NULL, " ");
+            char* transformations[7]; // 7 max commands??
+            int i = 0;
+            while(token != NULL){
+                transformations[i] = strdup(token);
+                printf("commands[%d] = %s\n", i, transformations[i]);
+                token = strtok(NULL, " ");
+                i++;  
+            }
+            transformations[i] = '\0';
+
+            /*
+            * APPLY TRANSFORMATIONS
+            */
+
+            transform(input_files, output_files, transformations);
         }
         if(strncmp(buffer, "status", 6) == 0){
             // DO THINGS
